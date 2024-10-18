097dcd5bf9ee33e7a17857bbb06543f35356b139
Dogan  Turkuler <35331566+dturkuler@users.noreply.github.com>
2024-10-19 00:42:34 +0300

    Create geocode.py

---
+++ geocode.py
@@ -0,0 +1,13 @@
+from geopy.geocoders import Nominatim
+
+def get_latitude_longitude(place):
+    geolocator = Nominatim(user_agent="your_app")
+    location = geolocator.geocode(place)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None, None
+
+place = "Ankara, Turkey"
+latitude, longitude = get_latitude_longitude(place)
+print(f"Latitude: {latitude}, Longitude: {longitude}")
